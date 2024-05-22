67d15c5d7f05756420c48f29bdbe84f775cac723
André Morales <an.dre.m@outlook.com>
2024-05-22 09:08:24 -0300

    refactor: Otimizado algoritmo de encontrar par
    Utilizando HashSet, foi melhorado findSum()

--- NumberFinder.java
+++ NumberFinder.java
@@ -1,17 +1,24 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NumberFinder {
     
     // Dado uma lista de numeros sem repeticoes "nums", encontre dois numeros que sua soma resulte em targetNum
     public static List<Integer> findSum(int targetNum, List<Integer> nums) {
+		// Set com todos os elementos de nums.
+		Set<Integer> otimoSet = new HashSet<>(nums);
+		
         for (int num1 : nums) {
-            for (int num2 : nums) {
-                int sum = num1 + num2;
-                if (sum == targetNum) {
-                    return List.of(num1, num2);
-                }
-            }
+			
+			int complemento = targetNum - num1;
+			
+			// Se o set contem o complemento, há a soma nesse set
+			if (otimoSet.contains(complemento)) {
+					
+				return List.of(num1, complemento);
+			}
         }
         
         return new LinkedList<>();
